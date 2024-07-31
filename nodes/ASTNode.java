1ee5f1e641ced7a932b605d0ff5b6a0a293058db
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-07-31 20:00:27 +0800

    feat:start to do check.

--- nodes/ASTNode.java
+++ nodes/ASTNode.java
@@ -1,7 +1,14 @@
 package nodes;
-
+import java.util.HashMap;
+import java.util.ArrayList;
 public class ASTNode {
   public int dim = 0;
   public String type = null;
+  public static HashMap<String, HashMap<String, Mypair>> class_memory = new HashMap<>();
+	 public static HashMap<String, HashMap<String, Mypair>> class_func_return = new HashMap<>();
+	 public static HashMap<String, HashMap<String, ArrayList<Mypair>>> class_func_args = new HashMap<>();
+	 public static HashMap<String, ArrayList<Mypair>> func_args = new HashMap<>();
+	 public static HashMap<String, Mypair> func_return = new HashMap<>();
+	 public static ArrayList<HashMap<String, Mypair>> variable_memory = new ArrayList<>();
   public void check() {}
 }
---
+++ nodes/Mypair.java
@@ -0,0 +1,8 @@
+public class Mypair {
+	public String type;
+	public Integer dim = 0;	
+	public Mypair(String t, int di) {
+		type = t;
+		dim = di;
+	}
+}
