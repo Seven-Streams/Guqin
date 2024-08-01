b1b46935eecf8e12d1cc02f5b2a7d247a5949e83
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-08-01 08:37:57 +0800

    fix:add the public tag.

--- nodes/FormatNode.java
+++ nodes/FormatNode.java
@@ -7,18 +7,18 @@ public class FormatNode extends ExprNode {
   HashMap<Integer, ASTNode> values = new HashMap<>();
 
   @Override
-  Mypair check() throws Exception {
+  public Mypair check() throws Exception {
     is_left = false;
     for (int i = 0;; i++) {
       if ((!values.containsKey(i)) && (!words.containsKey(i))) {
         break;
       } else {
-        if(values.containsKey(i)) {
+        if (values.containsKey(i)) {
           Mypair to_check = values.get(i).check();
-          if(to_check.dim != 0) {
+          if (to_check.dim != 0) {
             throw new Exception("Invalid dimension in format string.");
           }
-          if((!to_check.type.equals("int")) && (!to_check.type.equals("bool")) && (!to_check.equals("string"))) {
+          if ((!to_check.type.equals("int")) && (!to_check.type.equals("bool")) && (!to_check.equals("string"))) {
             throw new Exception("Invalid types in format string.");
           }
         }
