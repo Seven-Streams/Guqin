20e05516e679de92be4176b922fd040f16cf7238
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-08-16 10:53:51 +0800

    feat:finish the init, simplify and the coalesce of the conflict graph.

--- Optimization/LivenessAnalysis.java
+++ Optimization/LivenessAnalysis.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
+import java.util.Map;
 import IRSentence.*;
 import Composer.*;
 
@@ -17,7 +18,16 @@ public class LivenessAnalysis {
   public HashMap<Integer, HashMap<String, Boolean>> def = new HashMap<>();
   public HashMap<Integer, HashMap<String, Boolean>> in = new HashMap<>();
   public HashMap<Integer, HashMap<String, Boolean>> out = new HashMap<>();
+  public HashMap<String, HashMap<String, Boolean>> move_pair = new HashMap<>();
+  public HashMap<String, HashMap<String, Boolean>> coalesced_move_pair = new HashMap<>();
   public HashMap<String, HashMap<String, Boolean>> conflict_graph = new HashMap<>();
+  public HashMap<String, HashMap<String, Boolean>> simplified_graph = new HashMap<>();
+  public HashMap<String, String> coalesced_node = new HashMap<>();
+  //In coalesced_node, I'll show which the coalesced node attached to.
+  //When I coalesce 2 nodes, I'll change all the relative edges.
+  public Stack<String> selected = new Stack<>();
+  public HashMap<String, Boolean> move_related = new HashMap<>();
+  public HashMap<String, Boolean> now_move_related = new HashMap<>();
 
   public LivenessAnalysis(Composer _machine) {
     machine = _machine;
@@ -200,31 +210,31 @@ public class LivenessAnalysis {
           def_res.clear();
           use_res.clear();
           op.UseDefCheck(def_res, use_res);
-          for(String now_def:def_res.keySet()) {
+          for (String now_def : def_res.keySet()) {
             now_out.remove(now_def);
-            if(!conflict_graph.containsKey(now_def)) {
+            if (!conflict_graph.containsKey(now_def)) {
               conflict_graph.put(now_def, new HashMap<>());
             }
-            for(String out_v:now_out.keySet()) {
-              if(out_v.equals(now_def)) {
+            for (String out_v : now_out.keySet()) {
+              if (out_v.equals(now_def)) {
                 continue;
               }
-              if(!conflict_graph.containsKey(out_v)) {
+              if (!conflict_graph.containsKey(out_v)) {
                 conflict_graph.put(out_v, new HashMap<>());
               }
-              if(!conflict_graph.get(out_v).containsKey(now_def)) {
-                conflict_graph.get(out_v).put(now_def, false);
-                conflict_graph.get(now_def).put(out_v, false);
+              if (!conflict_graph.get(out_v).containsKey(now_def)) {
+                conflict_graph.get(out_v).put(now_def, null);
+                conflict_graph.get(now_def).put(out_v, null);
               }
             }
           }
-          for(String now_use:use_res.keySet()) {
+          for (String now_use : use_res.keySet()) {
             now_out.remove(now_use);
           }
         }
       }
       if (machine.generated.get(i) instanceof IRLabel) {
-        int now = ((IRLabel)(machine.generated.get(i))).label;
+        int now = ((IRLabel) (machine.generated.get(i))).label;
         Stack<IRCode> to_operate = new Stack<>();
         for (int j = i; j < machine.generated.size(); j++) {
           if (machine.generated.get(j) instanceof IRFunc || machine.generated.get(j) instanceof IRLabel
@@ -246,25 +256,25 @@ public class LivenessAnalysis {
           def_res.clear();
           use_res.clear();
           op.UseDefCheck(def_res, use_res);
-          for(String now_def:def_res.keySet()) {
+          for (String now_def : def_res.keySet()) {
             now_out.remove(now_def);
-            if(!conflict_graph.containsKey(now_def)) {
+            if (!conflict_graph.containsKey(now_def)) {
               conflict_graph.put(now_def, new HashMap<>());
             }
-            for(String out_v:now_out.keySet()) {
-              if(out_v.equals(now_def)) {
+            for (String out_v : now_out.keySet()) {
+              if (out_v.equals(now_def)) {
                 continue;
               }
-              if(!conflict_graph.containsKey(out_v)) {
+              if (!conflict_graph.containsKey(out_v)) {
                 conflict_graph.put(out_v, new HashMap<>());
               }
-              if(!conflict_graph.get(out_v).containsKey(now_def)) {
-                conflict_graph.get(out_v).put(now_def, false);
-                conflict_graph.get(now_def).put(out_v, false);
+              if (!conflict_graph.get(out_v).containsKey(now_def)) {
+                conflict_graph.get(out_v).put(now_def, null);
+                conflict_graph.get(now_def).put(out_v, null);
               }
             }
           }
-          for(String now_use:use_res.keySet()) {
+          for (String now_use : use_res.keySet()) {
             now_out.remove(now_use);
           }
         }
@@ -272,6 +282,14 @@ public class LivenessAnalysis {
       if (machine.generated.get(i) instanceof MoveBlock) {
         MoveBlock move = (MoveBlock) machine.generated.get(i);
         for (PseudoMove move_op : move.moves) {
+          if (!move_pair.containsKey(move_op.des)) {
+            move_pair.put(move_op.des, new HashMap<>());
+          }
+          if (!move_pair.containsKey(move_op.src)) {
+            move_pair.put(move_op.src, new HashMap<>());
+          }
+          move_related.put(move_op.des, null);
+          move_related.put(move_op.src, null);
           if (!conflict_graph.containsKey(move_op.des)) {
             conflict_graph.put(move_op.des, new HashMap<>());
           }
@@ -281,14 +299,95 @@ public class LivenessAnalysis {
             }
             if (!out_v.equals(move_op.des)) {
               if (out_v.equals(move_op.src)) {
-                conflict_graph.get(out_v).put(move_op.des, true);
-                conflict_graph.get(move_op.des).put(out_v, true);
+                conflict_graph.get(out_v).put(move_op.des, null);
+                conflict_graph.get(move_op.des).put(out_v, null);
               } else { // Move related
-                conflict_graph.get(out_v).put(move_op.des, false);
-                conflict_graph.get(move_op.des).put(out_v, false);
+                conflict_graph.get(out_v).put(move_op.des, null);
+                conflict_graph.get(move_op.des).put(out_v, null);
               }
+              move_related.put(out_v, null);
             }
           }
+        }
+      }
+    }
+    return;
+  }
+
+  void ConflictInit() {
+    simplified_graph.clear();
+    coalesced_move_pair.clear();
+    coalesced_node.clear();
+    now_move_related.clear();
+    for (Map.Entry<String, HashMap<String, Boolean>> node : conflict_graph.entrySet()) {
+      simplified_graph.put(node.getKey(), new HashMap<>());
+      HashMap<String, Boolean> op = simplified_graph.get(node.getKey());
+      for (Map.Entry<String, Boolean> pair : node.getValue().entrySet()) {
+        op.put(pair.getKey(), null);
+      }
+    } // Get a copy of the HashMap.
+
+    for (Map.Entry<String, HashMap<String, Boolean>> pairs : move_pair.entrySet()) {
+      coalesced_move_pair.put(pairs.getKey(), new HashMap<>());
+      HashMap<String, Boolean> op = coalesced_move_pair.get(pairs.getKey());
+      for (Map.Entry<String, Boolean> pair : pairs.getValue().entrySet()) {
+        op.put(pair.getKey(), null);
+      }
+    } // Get a copy of move_pairs.
+    for (String value : move_related.keySet()) {
+      now_move_related.put(value, null);
+    }
+  }
+
+  void ConflictSimplify(int degree) {
+    Queue<String> removes = new LinkedList<>();
+    for (Map.Entry<String, HashMap<String, Boolean>> node : simplified_graph.entrySet()) {
+      if ((node.getValue().size() < degree) && (!move_related.containsKey(node.getKey()))) {
+        removes.add(node.getKey());
+      }
+    }
+    while (!removes.isEmpty()) {
+      String remove = removes.poll();
+      HashMap<String, Boolean> remove_edge = simplified_graph.get(remove);
+      for (String des : remove_edge.keySet()) {
+        simplified_graph.get(des).remove(remove);
+      }
+      simplified_graph.remove(remove);
+      selected.push(remove);
+    }
+    return;
+  }
+
+  void ConflictCoalesce(int degree) {
+    for(Map.Entry<String, HashMap<String, Boolean>> node: coalesced_move_pair.entrySet()) {
+      for(String value: coalesced_move_pair.keySet()) {
+        boolean flag = true;
+        for(String edge: simplified_graph.get(node.getKey()).keySet()) {
+          if((simplified_graph.get(edge).size() < degree) || simplified_graph.get(edge).containsKey(value)) {
+            continue;
+          } else {
+            flag = false;
+            break;
+          }
+        }//George's rule.
+        if(flag) {
+          coalesced_node.put(value, node.getKey());//value is attached to node.
+          for(String res: coalesced_move_pair.get(node.getKey()).keySet()) {
+            coalesced_move_pair.get(res).remove(node.getKey());
+          }
+          for(String res:coalesced_move_pair.get(value).keySet()) {
+            coalesced_move_pair.get(res).remove(value);
+          }
+          coalesced_move_pair.remove(value);
+          coalesced_move_pair.remove(node.getKey());
+          now_move_related.remove(node.getKey());
+          now_move_related.remove(value);
+          for(String res: simplified_graph.get(value).keySet()) {
+            simplified_graph.get(res).remove(value);
+            simplified_graph.get(res).put(node.getKey(), null);
+            simplified_graph.get(node.getKey()).put(res, null);
+          }
+          simplified_graph.remove(value);
         }
       }
     }
