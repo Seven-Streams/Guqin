fc7d663bd07e0e85a7cbfa167090e8c8ad55fb16
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-09-13 16:55:30 +0800

    Revert "test:test another way of inline functions."
    This reverts commit e1805980afb0bb0fcb4782004209d6c1d6e94eb4.

--- Optimization/Inline.java
+++ Optimization/Inline.java
@@ -1,6 +1,5 @@
 package Optimization;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import IRSentence.*;
 import Composer.*;
@@ -9,18 +8,19 @@ public class Inline {
   Composer machine = null;
   HashMap<String, Boolean> ready_to_inline = new HashMap<>();
   HashMap<String, Integer> entry = new HashMap<>();
-  ArrayList<IRPhi> phis = new ArrayList<>();
 
   public Inline(Composer _machine) {
     machine = _machine;
   }
 
   public void Optim(int bound) throws Exception {
-    ready_to_inline.clear();
-    CheckGlobal();
-    FuncCheck(bound);
-    InlineFunc();
-    EmbeddingInline();
+    for(int i = 0; i < 2; i++) {
+      ready_to_inline.clear();
+      CheckGlobal();
+      FuncCheck(bound);
+      InlineFunc();
+      EmbeddingInline();
+    }
     return;
   }
 
@@ -34,7 +34,6 @@ public class Inline {
   }
 
   void FuncCheck(int bound) {
-    phis.clear();
     ready_to_inline.clear();
     entry.clear();
     String name = null;
@@ -47,10 +46,6 @@ public class Inline {
         entry.put(name, i);
         sentence_cnt = 1;
       }
-      if (code instanceof IRPhi) {
-        IRPhi phi = (IRPhi) code;
-        phis.add(phi);
-      }
       if (code instanceof IRFuncend) {
         if (sentence_cnt <= bound) {
           ready_to_inline.put(new String(name), true);
@@ -62,51 +57,23 @@ public class Inline {
   }
 
   void InlineFunc() throws Exception {
-    int now_cnt = 0;
-    int func_cnt = 0;
     for (int i = 0; i < machine.generated.size(); i++) {
       IRCode code = machine.generated.get(i);
-      if (code instanceof IRFunc) {
-        now_cnt = --func_cnt;
-      }
-      if (code instanceof IRLabel) {
-        now_cnt = ((IRLabel) code).label;
-      }
-      if (code instanceof InlineFunc) {
-        InlineFunc inline = (InlineFunc) code;
-        if (inline.operations.get(inline.operations.size() - 1) instanceof IRLabel) {
-          now_cnt = ((IRLabel) (inline.operations.get(inline.operations.size() - 1))).label;
-        } else {
-          now_cnt = ((IRLabel) (inline.operations.get(inline.operations.size() - 2))).label;
-        }
-      }
       if (code instanceof IRFuncall) {
         IRFuncall call = (IRFuncall) code;
         if (ready_to_inline.containsKey(call.func_name) && (ready_to_inline.get(call.func_name))) {
-          InlineFunc to_inline = GetInline(entry.get(call.func_name), call, now_cnt);
+          InlineFunc to_inline = GetInline(entry.get(call.func_name), call);
           machine.generated.set(i, to_inline);
-        }
-        code = machine.generated.get(i);
-        if (code instanceof InlineFunc) {
-          InlineFunc inline = (InlineFunc) code;
-          if (inline.operations.get(inline.operations.size() - 1) instanceof IRLabel) {
-            now_cnt = ((IRLabel) (inline.operations.get(inline.operations.size() - 1))).label;
-          } else {
-            now_cnt = ((IRLabel) (inline.operations.get(inline.operations.size() - 2))).label;
-          }
         }
       }
     }
     return;
   }
 
-  InlineFunc GetInline(int start, IRFuncall calling_info, int now) throws Exception {
+  InlineFunc GetInline(int start, IRFuncall calling_info) throws Exception {
     InlineFunc return_value = new InlineFunc();
     HashMap<Integer, Integer> label_replace = new HashMap<>();
     HashMap<String, String> name_replace = new HashMap<>();
-    return_value.operations.add(new IRjmp(++machine.label_number));
-    return_value.operations.add(new IRLabel(machine.label_number));
-    label_replace.put(-1, machine.label_number);
     IRFunc declar = (IRFunc) machine.generated.get(start);
     for (int i = 0; i < calling_info.reg.size(); i++) {
       name_replace.put(new String(declar.names.get(i)), new String(calling_info.reg.get(i)));
@@ -120,9 +87,6 @@ public class Inline {
     for (int i = (start + 1); i < machine.generated.size(); i++) {
       IRCode code = machine.generated.get(i);
       IRCode to_add = (code.GetInline(name_replace, label_replace, machine));
-      if (to_add instanceof IRPhi) {
-        phis.add((IRPhi) to_add);
-      }
       if (to_add != null) {
         return_value.operations.add(to_add);
       }
@@ -131,47 +95,6 @@ public class Inline {
       }
       if (code instanceof IRFuncend) {
         break;
-      }
-    }
-    if (calling_info.target_reg != null) {
-      IRPhi return_phi = new IRPhi();
-      return_phi.target = new String(calling_info.target_reg);
-      int now_label = 0;
-      for (int i = 0; i < return_value.operations.size(); i++) {
-        IRCode code = return_value.operations.get(i);
-        if (code instanceof IRLabel) {
-          IRLabel label = (IRLabel) code;
-          now_label = label.label;
-        }
-        if (code instanceof InlineFunc) {
-          InlineFunc inline = (InlineFunc) code;
-          if (inline.operations.get(inline.operations.size() - 1) instanceof IRLabel) {
-            now_label = ((IRLabel) (inline.operations.get(inline.operations.size() - 1))).label;
-          } else {
-            now_label = ((IRLabel) (inline.operations.get(inline.operations.size() - 2))).label;
-          }
-        }
-        if (code instanceof PseudoMove) {
-          return_phi.labels.add(now_label);
-          PseudoMove move = (PseudoMove) code;
-          return_phi.values.add(move.src);
-        }
-      }
-      for (int i = return_value.operations.size() - 1; i >= 0; i--) {
-        IRCode code = return_value.operations.get(i);
-        if (code instanceof PseudoMove) {
-          return_value.operations.remove(i);
-        }
-      }
-      return_value.operations.add(return_phi);
-      phis.add(return_phi);
-    }
-    for (IRPhi phi : phis) {
-      for (int i = 0; i < phi.labels.size(); i++) {
-        if (phi.labels.get(i) == now) {
-          phi.labels.set(i, label_replace.get(0));
-          break;
-        }
       }
     }
     return return_value;
@@ -183,15 +106,9 @@ public class Inline {
       if (code instanceof InlineFunc) {
         InlineFunc check = (InlineFunc) code;
         if (check.operations.size() > 1) {
-          if (check.operations.get(check.operations.size() - 1) instanceof IRLabel) {
-            IRLabel label = (IRLabel) check.operations.get(check.operations.size() - 1);
-            IRjmp res = new IRjmp(label.label);
-            check.operations.add(check.operations.size() - 1, res);
-          } else {
-            IRLabel label = (IRLabel) check.operations.get(check.operations.size() - 2);
-            IRjmp res = new IRjmp(label.label);
-            check.operations.add(check.operations.size() - 2, res);
-          }
+          IRLabel label = (IRLabel) check.operations.get(check.operations.size() - 1);
+          IRjmp res = new IRjmp(label.label);
+          check.operations.add(check.operations.size() - 1, res);
           machine.generated.addAll(i + 1, ((InlineFunc) code).operations);
         }
         machine.generated.remove(i);
